23cb724b5b32d24bdc19718c1fa1f8b4f6cd537f
pkuwalter <pkuwalter@qq.com>
2019-07-10 20:44:12 +0800

    iterColor

--- src/main.cpp
+++ src/main.cpp
@@ -110,6 +110,9 @@ int main(int argc, char *argv[])
     pr_gpu(g,gpu_num,value_gpu_pr,dsize,out_degree,copy_num,position_id);
 
     printf("The total color number is %d \n", countDistinct(value_gpu_pr, sizeof(value_gpu_pr)/sizeof(value_gpu_pr[0])));
+    int aaaaaa[10] = {0,1,1,2,2,3,3,5,5,6};
+
+    printf(countDistinct(aaaaaa), 10);
 
     free(g);
     free(out_degree);
