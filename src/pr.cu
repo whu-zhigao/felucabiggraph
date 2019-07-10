e574f8eda2b8ae8277c6c6380214a25d9670737f
pkuwalter <pkuwalter@qq.com>
2019-07-10 20:21:35 +0800

    iterColor

--- src/pr.cu
+++ src/pr.cu
@@ -92,7 +92,8 @@ static __global__ void pr_kernel_inner(
 
 		if(values[src] == values[dest])
 		{
-			delta = curand_uniform(&localState);			
+			delta = curand(&localState) % 100;	
+			printf("here is delta value %d \n", delta);		
 			atomicAdd(&add_values[dest],delta);	
 			printf("here is atomicAdd value %d \n", atomicAdd(&add_values[dest],delta));	
 		}
