1694c52e558046bfa2b76297df51772010ca6e13
pkuwalter <pkuwalter@qq.com>
2019-07-10 16:23:17 +0800

    iterColor

--- src/algorithm.h
+++ src/algorithm.h
@@ -10,13 +10,13 @@ Date: 29/01/16 11:30
 Description: This file defines the algorithm
 */
 
-void bfs_cpu(Graph_cpu *g,int *value_cpu,DataSize *dsize,int first_vertex);
+//void bfs_cpu(Graph_cpu *g,int *value_cpu,DataSize *dsize,int first_vertex);
 
 // print info about bfs values
-void print_bfs_values(const int * const values, int const size);
+//void print_bfs_values(const int * const values, int const size);
 
-void bfs_gpu(Graph **g,int gpu_num,int *value_gpu,DataSize *dsize, int first_vertex, int *copy_num, int **position_id);
+//void bfs_gpu(Graph **g,int gpu_num,int *value_gpu,DataSize *dsize, int first_vertex, int *copy_num, int **position_id);
 
-void pr_gpu(Graph **g,int gpu_num,float *value_gpu,DataSize *dsize, int* out_degree, int *copy_num, int **position_id);
+void pr_gpu(Graph **g,int gpu_num,int *value_gpu,DataSize *dsize, int* out_degree, int *copy_num, int **position_id);
 
 #endif
